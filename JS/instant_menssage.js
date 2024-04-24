71dcd30b43535690558b0c4ab90dc95d112c219a
Allan <69430214+allanlealluz@users.noreply.github.com>
2024-04-24 13:58:30 -0300

    rewrite of code

--- JS/instant_menssage.js
+++ JS/instant_menssage.js
@@ -1,151 +1,60 @@
-var div = document.getElementById('div')
-var form = document.getElementById('form')
-var input =  document.getElementById('conversa')
-var id = document.getElementById('id').innerHTML
-var form2 = document.getElementById('form2')    
-var button = document.getElementById('mandar')
-var nome = document.getElementById('nome').innerHTML
-var key = document.getElementById('key').innerHTML
-form.addEventListener('keypress',function(e){
-   if(e.key === 'Enter'){
-       button.click();
-   }
-})
-   //window.scrollBy(0, -window.innerHeight); 
-fetch(`/Chat/Carregar.php?id=${id}&arm=arm&key=${key}`)
-        .then(function(response){
-           return response.json()
-})
-        .then(function(data){
-         
-           for(var i in data){
-               console.log(data[i]['conversa'])
-       console.log(data[i]['fk_remetente'])
-             if(data[i]['fk_remetente'] === id){                                        
-                   if(data[i]['conversa'].substring(0,10) === 'data:image'){
-                       var img = document.createElement('img')
-                       img.src = data[i]['conversa'];
-                       img.setAttribute('id',"direita")
-                       img.setAttribute('class','img')
-                       div.appendChild(img)
-                   }else{
-                   var p = document.createElement('p')
-                   p.innerHTML = data[i]['conversa']                   
-                    p.setAttribute('class',"direita")
-                    p.setAttribute('id','direita');
-                   div.appendChild(p)
-               }
-                }else{
-                    if(data[i]['conversa'].substring(0,10) === 'data:image'){
-                       var img = document.createElement('img')
-                       img.src = data[0];
-                       img.setAttribute('id',"esquerda")
-                       img.setAttribute('class','img')
-                       div.appendChild(img)
-                   }else{
-                    var p = document.createElement('p')
-                     p.setAttribute('class',"esquerda")
-                   p.innerHTML = data[i]['conversa']
-                   div.appendChild(p) 
-               }
-                }               
-                 }                    
-})
-function buscar(){  
-    fetch(`/Chat/Carregar.php?id=${id}&key=${key}`)
-            .then(function(response){
-               return response.json()
-    })
-            .then(function(data){
-                for(var i in data){                 
-                    console.log(data[i]['conversa'])
-                   if(data[i]['conversa'].substring(0,10) === 'data:image' && data[i]['fk_remetente'] === id ){
-                       var img = document.createElement('img')
-                       img.src = data[i]['conversa'];
-                       img.setAttribute('class','img')
-                       img.setAttribute('class',"direita")
-                       div.appendChild(img)
-                   }
-                if(data[i]['fk_remetente'] === id && data[i]['conversa'].substring(0,10) !== 'data:image'){
-                   var p = document.createElement('p')
-                   p.innerHTML = data[i]['conversa']                   
-                    p.setAttribute('class',"direita")
-                    p.setAttribute('id','direita');
-                   div.appendChild(p)
-               }                          
-                        
-       
-                }      
-            
-             })   
-    
-    setTimeout('buscar()',500)
-}
-form.addEventListener('submit',function(e){
-    if(input.value != ''){
-    e.preventDefault()
-    var p =  document.createElement('p');
-    p.innerHTML = input.value;
-    p.setAttribute('class',"esquerda")
-    p.setAttribute('id',"esquerda")
-    div.appendChild(p)
-    fetch(`${id}`,{
-        method: 'POST',
-        body: input.value
-    })
-    fetch(`/Chat/Carregar.php?id=${id}&arm=arm&key=${key}`,{    
-        method : 'POST',
-        body: input.value
-    })
-            input.value = ''
-        }
-})
+const div = document.getElementById('div')
+const form = document.getElementById('form')
+const input = document.getElementById('conversa')
+const id = document.getElementById('id').innerHTML
+const form2 = document.getElementById('form2')
+const button = document.getElementById('mandar')
+const nome = document.getElementById('nome').innerHTML
+const key = document.getElementById('key').innerHTML
 
-form2.addEventListener('submit',function(e){
-     e.preventDefault()
-     var file = document.getElementById('file')
-     const leitorDeArquivos = new FileReader()
-     var input = file.files[0]
-     leitorDeArquivos.readAsDataURL(input)
-     leitorDeArquivos.addEventListener('loadend', function(load){
-         var img = document.createElement('img')
-        img.setAttribute('src',load.target.result)
-        img.width = 500
-        img.height = 500
-        div.appendChild(img)
-         fetch(`${id}`,{
-        method: 'POST',
-        body: load.target.result
-    })
-           file.value = ''
-     })  
-})
-function notifyMe() {
-    
-  // Verifica se o browser suporta notificações
-  if (!("Notification" in window)) {
-    alert("Este browser não suporta notificações de Desktop");
-  }
+form.addEventListener('keypress', (e) => e.key === 'Enter' && button.click())
 
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification("Hi there!");
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Hi there!");
-      }
-    });
-  }
-
-  // At last, if the user has denied notifications, and you
-  // want to be respectful there is no need to bother them any more.
+const loadMessages = async () => {
+  const response = await fetch(`/Chat/Carregar.php?id=${id}&arm=arm&key=${key}`)
+  const data = await response.json()
+  data.forEach(({ conversa, fk_remetente }) => {
+    const msg = document.createElement('p')
+    if (fk_remetente === id) msg.classList.add('direita')
+    else msg.classList.add('esquerda')
+    if (conversa.startsWith('data:image')) {
+      const img = document.createElement('img')
+      img.src = conversa
+      div.appendChild(img)
+    } else {
+      msg.textContent = conversa
+      div.appendChild(msg)
+    }
+  })
 }
 
+const sendMessage = async (message) => {
+  if (!message) return
+  const p = document.createElement('p')
+  p.textContent = message
+  p.classList.add('esquerda')
+  div.appendChild(p)
+  await fetch(`${id}`, { method: 'POST', body: message })
+  await fetch(`/Chat/Carregar.php?id=${id}&arm=arm&key=${key}`, { method: 'POST', body: message })
+  input.value = ''
+}
 
+form.addEventListener('submit', (e) => sendMessage(input.value))
 
+form2.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const file = document.getElementById('file').files[0]
+  const leitorDeArquivos = new FileReader()
+  leitorDeArquivos.readAsDataURL(file)
+  leitorDeArquivos.addEventListener('loadend', async () => {
+    const img = document.createElement('img')
+    img.src = leitorDeArquivos.result
+    img.width = 500
+    img.height = 500
+    div.appendChild(img)
+    await fetch(`${id}`, { method: 'POST', body: leitorDeArquivos.result })
+    document.getElementById('file').value = ''
+  })
+})
+
+loadMessages()
+setInterval(loadMessages, 500)
